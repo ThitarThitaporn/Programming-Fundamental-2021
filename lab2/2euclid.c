5b965b6ece263237acd00fc8b67e90281e3b0b13
ThitarThitaporn <64011100@KMITL.AC.TH>
2021-08-12 21:58:07 +0700

    first commit

---
+++ lab2/2euclid.c
@@ -0,0 +1,43 @@
+#include<stdio.h>
+int main()
+{
+    int num1,num2,A ;
+    printf("Enter first number : ");
+    scanf("%d",&num1);
+    printf("Enter second number : ");
+    scanf("%d",&num2);
+    //if(num1 < num2 )
+   while(num2 != 0)
+
+   {
+       A = num2;
+       num2 = num1%num2;
+       num1 = A;
+   }
+   printf("greatest common divisor : %d",num1);
+   return 0;
+   
+   
+   
+   
+    
+  
+}
+
+   
+   
+   
+   
+    //for(int i=2 ; i < num1 && i < num2 ;i++)
+    //while(num2 != 0)
+    //{
+       // if(num1 / i == 0 && num2 / i ==0)
+       // {
+        //    i*i =A;
+       //     i=1;
+      //  }
+   // }
+   // printf("%d",A);
+   // return 0;
+    
+//}
---
+++ lab2/tempCodeRunnerFile.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+int main()
+{
+    int num1,num2,A ;
+    printf("Enter first number : ");
+    scanf("%d",&num1);
+    printf("Enter second number : ");
+    scanf("%d",&num2);
+     if(num1<num2)
+     {
+        A=num1;
+     }
+     else
+    {
+        A=num2;
+    }
+    for(int i =A;i>=1;i--)
+    {
+        if(num1%i == 0 && num2%i == 0){
+            printf("%d",i);
+            break;
+        }
+    }
+    return 0;
+}
