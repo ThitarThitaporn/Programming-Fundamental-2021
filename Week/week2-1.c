9a9317a7b6835230cd79e0b3b23604915d74206e
ThitarThitaporn <64011100@KMITL.AC.TH>
2021-08-14 21:13:33 +0700

    commit

---
+++ Week/week2-1.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+int main()
+{
+    int num1, num2, num3, max1, max2;
+
+    printf("Enter number1 :");
+    scanf("%d", &num1);
+    printf("Enter number2 :");
+    scanf("%d", &num2);
+    printf("Enter number3 :");
+    scanf("%d", &num3);
+    
+    if (num1 + num2 > num2 + num3)
+    {
+        printf("The maximam %d", num1 + num2);
+    }
+    else if (num2 + num3 > num3 + num1)
+    {
+        printf("The maximam %d", num2 + num3);
+    }
+    else
+    {
+        printf("The maximam is%d", num3 + num1);
+    }
+    return 0;
+}
---
+++ Week/week2-2.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+int main()
+{
+    int num1, num2, num3;
+
+    printf("Enter number1 :");
+    scanf("%d", &num1);
+    printf("Enter number2 :");
+    scanf("%d", &num2);
+    printf("Enter number3 :");
+    scanf("%d", &num3);
+
+    if ((num1 >= num2) && (num2 >= num3))
+    {
+        printf("The maximam is %d", num1 + num2);
+    }
+    if ((num2 >= num3) && (num3 >= num1))
+    {
+        printf("The maximam is %d", num2 + num3);
+    }
+    if ((num3 >= num1) && (num1 >= num2))
+    {
+        printf("THe maximam is %d", num3 + num1);
+    }
+}
