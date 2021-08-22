68e40bf4a6b90f913cb8669fbfa7e03ff45f139a
ThitarThitaporn <64011100@KMITL.AC.TH>
2021-08-22 15:04:51 +0700

    week3

---
+++ Week/week3-1.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main()
+{
+    float num[5], sum = 0;
+    printf("Enter height\n");
+
+    for (int i = 0; i <= 4; i++)
+    {
+
+        scanf("%f", &num[i]);
+        sum += num[i];
+    }
+    sum /= 5;
+    printf("%.2f %.2f %.2f %.2f %.2f", num[0], num[1], num[2], num[3], num[4]);
+    printf("\nThe average is %.2f", sum);
+    return 0;
+}
---
+++ Week/week3-2.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(){
+    float  a,b,c,d,e,x;
+    printf("Enter height\n");
+    scanf("%f\n%f\n%f\n%f\n%f", &a,&b,&c,&d,&e);
+    printf("%.2f %.2f %.2f %.2f %.2f",a,b,c,d,e);
+    x=(a+b+c+d+e)/5;
+	printf("\nThe average is %.2f",x);
+    return 0;	
+
+}
