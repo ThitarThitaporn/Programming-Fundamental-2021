0f98d9dccb6a3cc2a4710ba38a5b6af6a3791e87
ThitarThitaporn <64011100@KMITL.AC.TH>
2021-08-31 14:33:42 +0700

    ac4

--- lab/lab4_1.c
+++ lab/lab4_1.c
@@ -6,7 +6,6 @@ int main()
     scanf_s("%f %f %f", &l1, &l2, &l3);
     s = (l1 + l2 + l3) / 2;
     area = sqrt((s * (s - l1)) * (s - l2) * (s - l3));
-
     if ((l1 + l2 > l3) && (l1 + l3 > l2) && (l2 + l3 > l1))
     {
         printf("%f", area);
