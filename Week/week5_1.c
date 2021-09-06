01772f232ee79bd8a4bb94e08eeaf21d7943cdd0
ThitarThitaporn <64011100@KMITL.AC.TH>
2021-09-06 11:13:23 +0700

    2nd

--- Week/week5_1.c
+++ Week/week5_1.c
@@ -1,8 +1,16 @@
 // มีคนทั้งหมด n คน ทุกคนต้องการแลกบัตรอวยพรให้กันและกัน ต้องใช้บัตรทั้งหมดกี่ใบ (Level 3)
 #include<stdio.h>
+int ExchCard(int);
 int main(){
-     int num;
+    int num;
      scanf_s("%d",&num);
-     printf("%d",num*(num-1));
-     return 0;
+     ExchCard(num) ;  
+}
+
+int ExchCard (int i)
+{
+    i = i*(i-1);
+    printf("%d",i);
+    return 0;
+
 }
