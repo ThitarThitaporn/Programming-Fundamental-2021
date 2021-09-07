b77eaa707640ef256354edc5336638fabb404a97
ThitarThitaporn <64011100@KMITL.AC.TH>
2021-09-08 00:29:40 +0700

    1st

---
+++ ac/64011100_Ac5.c
@@ -0,0 +1,28 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+int main()
+{
+	char str[50];
+	char *ptr;
+
+	ptr = str;
+	scanf("%s", str);
+	printf("Input : %s\n",str);
+	printf("Output : ");
+	while (*ptr != '\0')
+
+	{
+			if (*ptr != *(ptr+1))
+			{
+				printf("%c", *ptr);
+				ptr++;
+			}
+
+			else
+			{
+				ptr++;
+			}
+		
+	}
+	return 0;
+}
