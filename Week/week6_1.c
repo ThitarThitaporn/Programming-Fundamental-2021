6aac2016b9a9fd20fa3d598257a3dacafdefd38c
ThitarThitaporn <64011100@KMITL.AC.TH>
2021-09-12 09:37:38 +0700

    3

--- Week/week6_1.c
+++ Week/week6_1.c
@@ -1,16 +1,15 @@
 #include <stdio.h>
 int main()
 {
-    int n,s, m, h;
+    int n, s, m, h;
     int m0 = 0, s0 = 0;
-
     printf("Input\n");
     scanf("%d", &n);
     h = n / 3600;
-    m= (n/60)%60;
+    m = (n / 60) % 60;
     s = n % 60;
     printf("Output\n");
-   printf("%d:%02d:%02d",h,m,s);
+    printf("%d:%02d:%02d", h, m, s);
 
     return 0;
 }
--- Week/week6_2.c
+++ Week/week6_2.c
@@ -1,32 +1,33 @@
-#include<stdio.h>
-int hour(int n,int h){
-     h = n / 3600;
-     printf("%d:",h);
-     
+#include <stdio.h>
+int hour(int n, int h)
+{
+    h = n / 3600;
+    printf("%d:", h);
 }
 
-int min(int n,int m){
+int min(int n, int m)
+{
 
-    m = (n/60)%60;
-     printf("%02d:",m);
-    
+    m = (n / 60) % 60;
+    printf("%02d:", m);
 }
 
-int second (int n,int s){
-    s=n%60;
- printf("%02d",s);
+int second(int n, int s)
+{
+    s = n % 60;
+    printf("%02d", s);
 }
 
 int main()
-{   
-    int n,h,m,s;
+{
+    int n, h, m, s;
     int m0 = 0, s0 = 0;
     printf("Input\n");
     scanf("%d", &n);
     printf("Output\n");
-    hour(n,h);
-    min(n,m);
-    second(n,s);
+    hour(n, h);
+    min(n, m);
+    second(n, s);
 
     return 0;
 }
