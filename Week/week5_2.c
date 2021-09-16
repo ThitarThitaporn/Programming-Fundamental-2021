4feaa75136f6efb6965d16cfa2ff10bbfc7d7c87
ThitarThitaporn <64011100@KMITL.AC.TH>
2021-09-16 14:14:46 +0700

    1

--- Week/week5_2.c
+++
@@ -1,8 +0,0 @@
-// มีคนทั้งหมด n คน ทุกคนต้องการแลกบัตรอวยพรให้กันและกัน ต้องใช้บัตรทั้งหมดกี่ใบ (Level 3)
-#include<stdio.h>
-int main(){
-     int num;
-     scanf_s("%d",&num);
-     printf("%d",num*(num-1));
-     return 0;
-}
