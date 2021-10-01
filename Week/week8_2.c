f7731aa225f8ea3e96dce08f4f417934a71e21f8
ThitarThitaporn <64011100@KMITL.AC.TH>
2021-10-01 15:29:15 +0700

    2

--- Week/week8_2.c
+++ Week/week8_2.c
@@ -1,15 +1,15 @@
 // จงเขียนโปรแกรม Copy ไฟล์ข้อมูลโดยให้นักศึกษาป้อนข้อมูลของไฟล์ต้นทาง และข้อมูลไฟล์ปลายทาง (Level 4)
-#include <stdio.h>
+#include<stdio.h>
 int main()
 {
-    FILE *fp;
-    char in[100];
-    printf("Input: ");
-    scanf("%[^\n]", in); 
-     FILE *inputFile; fp  = fopen(in, "r"); 
-    if (fp==(FILE*) NULL) 
+    FILE *fp ;
+    char in[100] ;
+    printf("Input : ");
+    scanf("%[^\n]", in);
+    fp = fopen(in, "r");
+    if(fp == (FILE *)NULL) 
     {
-        printf("There is no such file.");
+        printf("There is no such file."); 
         return 0;
     }
 
@@ -17,9 +17,9 @@ int main()
     printf("Output: ");
     scanf(" %[^\n]", out); 
     FILE *outFile = fopen(out, "w");
-    
+
     char ch;
-    while ((ch = fgetc(inputFile)) != EOF)
+    while ((ch = fgetc(fp)) != EOF)
     {
         fprintf(outFile, "%c", ch);
     }
